1a7cb5f60b1f10a21c73509cffa057c5860fd118
ajys3107 <156227292+ajys3107@users.noreply.github.com>
2024-05-08 00:18:48 +0530

    factorial

---
+++ factorial.java
@@ -0,0 +1,14 @@
+class Factorial {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            
+            factorial *= i;
+        }
+        System.out.println("The Factorial of Number "+num+" is "+factorial);
+    }
+}
