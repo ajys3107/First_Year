37a16cd2c0eb8912e79a75c28aca6cbaa9ff5bab
ajys3107 <156227292+ajys3107@users.noreply.github.com>
2024-05-08 00:18:16 +0530

    even and odd

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+class EvenOdd{
+	public static void main(String args[]){
+		int a=4;
+		if (a%2==0){
+			System.out.println("The number "+a+" is even");
+		}
+		else{
+			System.out.println("The number "+a+" is odd");
+		}
+	}
+}
